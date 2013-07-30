f69e609c0d67002259bc43748404249185f74945
Sharkman <sharkman.ru@gmail.com>
2013-07-31 01:59:17 +0400

    Added helpful command to find portable git installation on Windows

---
+++ Packages/User/FindGitPortable.py
@@ -0,0 +1,16 @@
+
+import sublime
+import sublime_plugin
+import os
+import glob
+
+class FindGitPortableBinaryCommand(sublime_plugin.WindowCommand):
+    """
+    Search for actual path for a portable git, that comes with the official
+    GitHub application (this path changes every time the application updates)
+    """
+    def run(self, **kwargs):
+        for d in glob.glob(os.path.join(os.environ["LOCALAPPDATA"], "GitHub", "PortableGit_*")):
+            sublime.load_settings("Git.sublime-settings").set("git_command", os.path.join(d, "bin", "git"))
+            sublime.save_settings("Git.sublime-settings")
+            break
